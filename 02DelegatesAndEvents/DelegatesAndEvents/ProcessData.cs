82093cbc93482bd1aa351ccd77f45ff6556922c5
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-04-15 15:54:40 -0400

    4.6. Demo: Using Action<T>

---
+++ 02DelegatesAndEvents/DelegatesAndEvents/ProcessData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public class ProcessData
+    {
+        public void Process(int x, int y, BizRulesDeleate del)
+        {
+            var result = del(x, y);
+            Console.WriteLine(result);
+        }
+
+        public void ProcessAction(int x, int y, Action<int, int> action)
+        {
+            action(x, y);
+            Console.WriteLine("Action has been processed");
+        }
+    }
+}
--- 02DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ 02DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DelegatesAndEvents
 {
@@ -9,11 +11,25 @@ namespace DelegatesAndEvents
         GenerateReports
     }
 
+    public delegate int BizRulesDeleate(int x, int y);
+
     internal class Program
     {
 
         private static void Main(string[] args)
         {
+            BizRulesDeleate addDel = (x, y) => x + y;
+            BizRulesDeleate multiplyDel = (x, y) => x * y;
+
+            var data = new ProcessData();
+            data.Process(2, 3, addDel);
+            data.Process(2, 3, multiplyDel);
+
+            Action<int, int> myAction = (x, y) => Console.WriteLine("add action " + (x+y)) ;
+            Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine("multiply action " + (x*y)) ;
+            data.ProcessAction(2, 3, myAction);
+            data.ProcessAction(2, 3, myMultiplyAction);
+
             var worker = new Worker();
             //            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(WorkPerformed);
             //            worker.WorkCompleted += new EventHandler(WorkCompleted);
@@ -26,24 +42,32 @@ namespace DelegatesAndEvents
             //            worker.WorkCompleted -= WorkCompleted;
 
             // anonymous methods:
-            worker.WorkPerformed +=
-                delegate (object o, WorkPerformedEventArgs e)
-                {
-                    Console.WriteLine($"Work Performed: {e.Hours} hour(s) of {e.WorkType}");
-                };
-            worker.WorkCompleted += delegate(object o, EventArgs e) { Console.WriteLine("Work completed"); };
+//            worker.WorkPerformed +=
+//                delegate (object o, WorkPerformedEventArgs e)
+//                {
+//                    Console.WriteLine($"Work Performed: {e.Hours} hour(s) of {e.WorkType}");
+//                };
+//            worker.WorkCompleted += delegate(object o, EventArgs e) { Console.WriteLine("Work completed"); };
+
+            // lambdas
+            worker.WorkPerformed += (o, e) =>
+            {
+                Console.WriteLine($"Work Performed: {e.Hours} hour(s) of {e.WorkType}");
+                Console.WriteLine("This line just shows how you can have multiple lines of code in a lambda.");
+            };
+            worker.WorkCompleted += (o, e) => Console.WriteLine("Work completed");
 
             worker.Dowork(8, WorkType.GenerateReports);
         }
 
-        private static void WorkCompleted(object o, EventArgs e)
-        {
-            Console.WriteLine("Work completed");
-        }
-
-        private static void WorkPerformed(object o, WorkPerformedEventArgs e)
-        {
-            Console.WriteLine($"Work Performed: {e.Hours} hour(s) of {e.WorkType}");
-        }
+//        private static void WorkCompleted(object o, EventArgs e)
+//        {
+//            Console.WriteLine("Work completed");
+//        }
+//
+//        private static void WorkPerformed(object o, WorkPerformedEventArgs e)
+//        {
+//            Console.WriteLine($"Work Performed: {e.Hours} hour(s) of {e.WorkType}");
+//        }
     }
 }
