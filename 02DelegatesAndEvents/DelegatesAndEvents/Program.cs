ce2fcb39225577a4832e9c1060ee28947208ba1e
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-04-15 12:26:31 -0400

    2. Creating Delegates, Events, and EventArgs

--- 02DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ 02DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -6,10 +6,69 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+
+
     class Program
     {
+        public delegate int WorkPerformedHandler(int hours, WorkType workType);
+
         static void Main(string[] args)
         {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+
+//            del1(5, WorkType.Golf);
+//            del2(10, WorkType.GenerateReports);
+
+//            DoWork(del1);
+
+//            del1 += del2 + del3;
+//            int finalHours = del1(7, WorkType.GenerateReports);
+//            Console.WriteLine(finalHours);
+
+            Worker worker = new Worker();
+//            worker.WorkPerformed += new DelegatesAndEvents.WorkPerformedHandler(WorkPerformed);
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(WorkPerformed);
+            worker.WorkCompleted += new EventHandler(WorkCompleted);
+            worker.Dowork(3, WorkType.GoToMeetings);
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GoToMeetings);
+        }
+
+        static int WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed1 called. {hours} hours and {workType.ToString()} workType");
+            return hours + 1;
+        }
+        static int WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed2 called. {hours} hours and {workType.ToString()} workType");
+            return hours + 2;
+        }
+        static int WorkPerformed3(int hours, WorkType workType)
+        {
+            Console.WriteLine($"WorkPerformed3 called. {hours} hours and {workType.ToString()} workType");
+            return hours + 3;
+        }
+
+        static void WorkCompleted(Object o, EventArgs e)
+        {
+            Console.WriteLine("Work completed");
+        }
+
+        static void WorkPerformed(Object o, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine($"Work Performed: {e.Hours} hour(s) of {e.WorkType}");
         }
     }
 }
---
+++ 02DelegatesAndEvents/DelegatesAndEvents/Worker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+//    public delegate void WorkPerformedHandler(object sender, WorkPerformedEventArgs e);
+
+    public class Worker
+    {
+//        public event WorkPerformedHandler WorkPerformed;
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        public EventHandler WorkCompleted;
+
+        public void Dowork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+                OnWorkPerformed(i+1, workType);
+            }
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+//            if (WorkPerformed != null) WorkPerformed(this, new WorkPerformedEventArgs(hours, workType));
+//            var del = WorkPerformed as WorkPerformedHandler;
+            var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
+            if (del != null) del(this, new WorkPerformedEventArgs(hours, workType)); 
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+//            WorkCompleted?.Invoke(this, EventArgs.Empty);
+
+            var del = WorkCompleted as EventHandler;
+            del?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
